4ce3d301f69f2c2b66adc07130f2fd3bcf60cdde
voxelit <noxeleria@gmail.com>
2024-02-11 18:20:55 -0800

    ci lab

---
+++ src/counter.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+# we need to import the file that contains the status codes
+from src import status
+
+app = Flask(__name__)
+
+COUNTERS = {}
+
+# We will use the app decorator and create a route called slash counters.
+# specify the variable in route <name>
+# let Flask know that the only methods that is allowed to called
+# on this function is "POST".
+@app.route('/counters/<name>', methods=['POST'])
+def create_counter(name):
+    """Create a counter"""
+    app.logger.info(f"Request to create counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+      return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    COUNTERS[name] = 0
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+@app.route('/counters/<name>', methods=['PUT'])
+def update_counter(name):
+    """Update a counter"""
+    app.logger.info(f"Request to update counter: {name}")
+    if name in COUNTERS:   # if counter does not exist, return error
+        COUNTERS[name] += 1     # increment counter by 1
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
+    else:
+        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+
+@app.route('/counters/<name>', methods=['GET'])
+def get_counter(name):
+    """Get a counter"""
+    app.logger.info(f"Request to get counter: {name}")
+    if name in COUNTERS:   # return counter and 200 status message if found
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
+    else:
+        return {"Message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,58 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app
+
+# we need to import the file that contains the status codes
+from src import status
+
+class CounterTest(TestCase):
+    """Counter tests"""
+
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        client = app.test_client()
+        result = client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        """It should update a counter"""
+        result = self.client.post('/counters/test')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        initialVal = result.json['test']
+        updateTest = self.client.put('/counters/test')
+        self.assertEqual(updateTest.status_code, status.HTTP_200_OK)
+        updateValue = updateTest.json['test']
+        self.assertEqual(initialVal + 1, updateValue)
+
+    def test_update_a_bad_counter(self):
+        """It should return an error for updating non-existent counter"""
+        updateTest = self.client.put('/counters/test')
+        self.assertEqual(updateTest.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_a_counter(self):
+        """It should get a counter"""
+        result = self.client.post('/counters/newTest')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        value = self.client.get('/counters/newTest')
+        self.assertEqual(value.status_code, status.HTTP_200_OK)
+        self.assertEqual(value.json['newTest'], 0)
+
+    def test_get_a_bad_counter(self):
+        """It should return an error for getting non-existent counter"""
+        updateTest = self.client.get('/counters/test')
+        self.assertEqual(updateTest.status_code, status.HTTP_404_NOT_FOUND)
